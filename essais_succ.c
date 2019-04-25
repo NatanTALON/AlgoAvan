fa2e124e013a80aaef9ec9c7ecfcb870e63e8783
NatanTALON <natan.talon@gmail.com>
2019-04-25 22:58:18 +0200

    ajout de l'approche par essais successifs

---
+++ essais_succ.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Point.h"
+#include "parser.h"
+#include "distance.h"
+#include "writer.h"
+
+
+void appligbri(int i, int dernierPointAccepte);
+
+Point *points = NULL;
+int nb_points = 0;
+int *Xopt;
+int *X;
+int nb1dansX = 1;
+double C = 1.5;
+double Copt;
+double Cact = 0.0;
+
+
+int main(int argc, char* argv[]) {
+	if(argc < 2) {
+		printf("Erreur : veuillez spécifier le fichier à lire\n");
+		return 0;
+	} else if (argc == 3) {
+		C = atof(argv[2]);
+		printf("C = %f\n", C);
+	}
+
+	parser(argv[1], &points, &nb_points);
+
+	if(nb_points >= 2) {
+		Xopt = malloc(nb_points * sizeof(int));
+		Xopt[0] = 1;
+		int j;
+		for(j = 1; j < nb_points-1; j++) {
+			Xopt[j] = 0;
+		}
+		Xopt[nb_points-1] = 1;
+		X = malloc(nb_points * sizeof(int));
+		X[0] = 1;
+		X[nb_points-1] = 1;
+		Copt = C + distance(0, nb_points-1, points);
+	} else {
+		printf("Erreur lors de la lecture du fichier\n");
+		return 0;
+	}
+
+	appligbri(1, 0);
+
+	printf("\nCopt = %f\n", Copt);
+
+	writeFile("test_python.txt", points, Xopt, nb_points);
+
+	free(points);
+	free(Xopt);
+	free(X);
+
+	return 0;
+}
+
+
+void appligbri(int i, int dernierPointAccepte) {
+	if(i < nb_points) {
+		if(Cact + distance(dernierPointAccepte, i, points) < Copt) {
+			X[i] = 1;
+			nb1dansX++;
+			Cact += distance(dernierPointAccepte, i, points);
+
+			if(Cact + C*(nb1dansX-1) < Copt) {
+				if(i == nb_points-1) {
+					int j;
+					for(j = 0; j < nb_points; j++) {
+						Xopt[j] = X[j];
+					}
+					Copt = Cact + C*(nb1dansX-1);
+				} else {
+					appligbri(i+1, i);
+				}
+			}
+
+			X[i] = 0;
+			nb1dansX--;
+			Cact -= distance(dernierPointAccepte, i, points);
+			appligbri(i+1, dernierPointAccepte);
+		} else {
+			X[i] = 0;
+			appligbri(i+1, dernierPointAccepte);
+		}
+	}
+}
